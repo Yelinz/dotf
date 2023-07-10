6df244a7ea91d660d145323d5ff710b88b836c2f
Yelin Zhang <30687616+Yelinz@users.noreply.github.com>
2023-07-10 15:03:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ read -p 'Email: ' email
 
 # general dependencies
 # libfuse2 is for appimages
-sudo apt-get install git curl libfuse2 ripgrep
+sudo apt-get install git curl libfuse2 ripgrep ca-certificates gnupg
 
 # nvim setup
 
@@ -19,3 +19,19 @@ git config --global user.email $email
 # node
 curl -L https://bit.ly/n-install | N_PREFIX=~/tools/n bash -s -- -y
 npm install -g npm yarn pnpm
+
+# docker
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# poetry
+curl -sSL https://install.python-poetry.org | python3 -
+poetry completions fish > ~/.config/fish/completions/poetry.fish
+set PATH $PATH /home/yz/.local/bin
