f6dbb08824a4a25a53cdf60353d31e3af1164385
Yelin Zhang <30687616+Yelinz@users.noreply.github.com>
2023-07-10 14:17:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+cd ~
+mkdir repos tools
+
 read -p 'Email: ' email 
 
 # general dependencies
+# libfuse2 is for appimages
 sudo apt-get install git curl libfuse2 ripgrep
 
 # nvim setup
@@ -11,3 +15,7 @@ sudo apt-get install git curl libfuse2 ripgrep
 git config --global core.editor "vim"
 git config --global user.name "Yelin Zhang"
 git config --global user.email $email
+
+# node
+curl -L https://bit.ly/n-install | N_PREFIX=~/tools/n bash -s -- -y
+npm install -g npm yarn pnpm
